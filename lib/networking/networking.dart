50db483f622c1ad2e53720a37f3bad5260f77399
eRuaro <erickson_ruaroii@dlsu.edu.ph>
2021-06-27 12:06:29 +0800

    intial functions

--- lib/networking/networking.dart
+++ lib/networking/networking.dart
@@ -1,8 +1,17 @@
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 class Networking {
   Map<String, String> _headers = {
     "x-rapidapi-key": "79cea3149emshe7cff09ba0dcd65p1ac7dejsn9697c25e50fd",
     "x-rapidapi-host": "shazam.p.rapidapi.com",
   };
+
+  var _recommendedSongsUrl = Uri.parse("https://shazam.p.rapidapi.com/songs/list-recommendations?key=484129036");
+
+  Future getRecommendedSongs() async {
+    http.Response response = await http.get(_recommendedSongsUrl);
+  }
+  Future getSongs() async {
+    http.Response response = await http.get(Uri.parse(_headers['x-rapidapi-host']!));
+  }
 }
