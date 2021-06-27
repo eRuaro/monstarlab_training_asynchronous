509bc434c167d74364b1d75b42f470430902d41d
eRuaro <erickson_ruaroii@dlsu.edu.ph>
2021-06-27 11:52:02 +0800

    added keys

---
+++ lib/networking/networking.dart
@@ -0,0 +1,8 @@
+import 'package:http/http.dart';
+
+class Networking {
+  Map<String, String> _headers = {
+    "x-rapidapi-key": "79cea3149emshe7cff09ba0dcd65p1ac7dejsn9697c25e50fd",
+    "x-rapidapi-host": "shazam.p.rapidapi.com",
+  };
+}
