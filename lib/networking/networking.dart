30100439f7784f3bb53ae613fc3834dff4d82875
eRuaro <erickson_ruaroii@dlsu.edu.ph>
2021-06-27 12:18:48 +0800

    protected api keys, added dio package

--- lib/networking/networking.dart
+++ lib/networking/networking.dart
@@ -1,10 +1,8 @@
 import 'package:http/http.dart' as http;
+import 'package:monstarlab_training_asynchronous/api_keys.dart';
+
 
 class Networking {
-  Map<String, String> _headers = {
-    "x-rapidapi-key": "79cea3149emshe7cff09ba0dcd65p1ac7dejsn9697c25e50fd",
-    "x-rapidapi-host": "shazam.p.rapidapi.com",
-  };
 
   var _recommendedSongsUrl = Uri.parse("https://shazam.p.rapidapi.com/songs/list-recommendations?key=484129036");
 
@@ -12,6 +10,6 @@ class Networking {
     http.Response response = await http.get(_recommendedSongsUrl);
   }
   Future getSongs() async {
-    http.Response response = await http.get(Uri.parse(_headers['x-rapidapi-host']!));
+    http.Response response = await http.get(Uri.parse(headers['x-rapidapi-host']!));
   }
 }
